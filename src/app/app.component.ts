9a1babce4b37fc46644d278620f3cf31e220d0f7
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2025-12-02 23:22:24 +0530

    post & get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,23 @@ export class AppComponent implements OnInit {
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+   this.http.post('https://http-check-902dd-default-rtdb.firebaseio.com/posts.json', postData).subscribe(res => {
+    console.log('your response data == ', res);
+   });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get('https://http-check-902dd-default-rtdb.firebaseio.com/posts.json').subscribe(resData => {
+      console.log('resData -- ', resData);
+    })
   }
 }
